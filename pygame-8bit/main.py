58f75492711f64f5bf233b85dafedc227fa1633f
Marco <marco.pertegal.prieto@gmail.com>
2024-01-25 23:38:43 +0100

    New proyect

---
+++ pygame-8bit/main.py
@@ -0,0 +1,62 @@
+import pygame
+from skins import *
+from config import *
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((1100, 620))
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def new(self):
+        self.playing = True
+
+        self.all_skins = pygame.sprite.LayeredUpdates()
+        self.rocks = pygame.sprite.LayeredUpdates()
+
+        self.player = Player(self, 1, 2)
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+
+    def update(self):
+        self.all_skins.update()
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_skins.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+
+    def main(self):
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
+
+    def game_over(self):
+        pass
+
+    def intro_screen(self):
+        pass
+
+
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
+
+pygame.quit()
+sys.exit()
