c8b388162d55138665e01f3047b1e75063c6f6cc
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2024-01-26 12:36:20 +0100

    Rebuild proyect

--- pygame-8bit/main.py
+++ pygame-8bit/main.py
@@ -3,8 +3,36 @@ from skins import *
 from config import *
 import sys
 
-pygame.init()
-screen = pygame.display.set_mode((1100, 620))
+
+class Spritesheet:
+    def __init__(self, file):
+        self.sheet = pygame.image.load(file).convert()
+
+    def get_sprite(self, x, y, width, height):
+        sprite = pygame.Surface([width, height])
+        sprite.blit(self.sheet, (0, 0), (x, y, width, height))
+        return sprite
+
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = 1
+        self.groups = self.game.all_skins
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+        self.width, self.height = TILESIZE, TILESIZE
+        self.image = self.game.groundsheet.get_sprite(0, 96, self.width, self.height)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = self.x, self.y
+
+
+def build_map(self, tilemap):
+    for i, row in enumerate(tilemap):
+        for j, column in enumerate(row):
+            Ground(self, j, i)
 
 
 class Game:
@@ -13,14 +41,19 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
+        self.groundsheet = Spritesheet('assets/ground.png')
 
-    def new(self):
+    def createTileMap(self, tilemap):
+        build_map(self, tilemap)
+
+    def new(self, tilemap):
         self.playing = True
-
         self.all_skins = pygame.sprite.LayeredUpdates()
-        self.rocks = pygame.sprite.LayeredUpdates()
+        self.createTileMap(tilemap)
 
-        self.player = Player(self, 1, 2)
+        """
+        self.rocks = pygame.sprite.LayeredUpdates()
+        self.player = Player(self, 1, 2)"""
 
     def events(self):
         for event in pygame.event.get():
@@ -32,7 +65,7 @@ class Game:
         self.all_skins.update()
 
     def draw(self):
-        self.screen.fill(BLACK)
+        self.screen.fill("black")
         self.all_skins.draw(self.screen)
         self.clock.tick(FPS)
         pygame.display.update()
@@ -42,7 +75,8 @@ class Game:
             self.events()
             self.update()
             self.draw()
-        self.running = False
+
+        """self.running = False"""
 
     def game_over(self):
         pass
@@ -51,12 +85,9 @@ class Game:
         pass
 
 
-g = Game()
-g.intro_screen()
-g.new()
-while g.running:
-    g.main()
-    g.game_over()
+game = Game()
+game.new(TILEMAP)
+while game.running:
+    game.main()
 
 pygame.quit()
-sys.exit()
