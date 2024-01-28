bff40caa648523fa6114e79fcea5e1f2670fa65d
Marco <marco.pertegal.prieto@gmail.com>
2024-01-28 23:32:53 +0100

    New clases and player location

--- pygame-8bit/main.py
+++ pygame-8bit/main.py
@@ -1,39 +1,12 @@
 import pygame
-from skins import *
 from config import *
+from sounds import *
+from player import *
+from map_build import *
+import maps
+from sprites import *
+
 import sys
-
-
-class Spritesheet:
-    def __init__(self, file):
-        self.sheet = pygame.image.load(file).convert()
-
-    def get_sprite(self, x, y, width, height):
-        sprite = pygame.Surface([width, height])
-        sprite.blit(self.sheet, (0, 0), (x, y, width, height))
-        return sprite
-
-
-class Ground(pygame.sprite.Sprite):
-    def __init__(self, game, x, y):
-        self.game = game
-        self._layer = 1
-        self.groups = self.game.all_skins
-        pygame.sprite.Sprite.__init__(self, self.groups)
-
-        self.x = x * TILESIZE
-        self.y = y * TILESIZE
-        self.width, self.height = TILESIZE, TILESIZE
-        self.image = self.game.groundsheet.get_sprite(0, 96, self.width, self.height)
-        self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = self.x, self.y
-
-
-def build_map(self, tilemap):
-    for i, row in enumerate(tilemap):
-        for j, column in enumerate(row):
-            Ground(self, j, i)
-
 
 class Game:
     def __init__(self):
@@ -41,8 +14,8 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
-        self.groundsheet = Spritesheet('assets/ground.png')
-
+        self.groundsheet = Spritesheet('assets/gound4.png')
+        self.character_spritesheet = Spritesheet('assets/ground.png')
     def createTileMap(self, tilemap):
         build_map(self, tilemap)
 
@@ -84,7 +57,8 @@ class Game:
     def intro_screen(self):
         pass
 
-
+#Creacion del mapa
+TILEMAP = maps.world_1.stage_1
 game = Game()
 game.new(TILEMAP)
 while game.running:
