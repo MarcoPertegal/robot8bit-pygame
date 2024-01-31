7d7840c79a8c6aee4051e6689febae721d6881f3
Marco <marco.pertegal.prieto@gmail.com>
2024-01-31 18:39:45 +0100

    intro screen

--- pygame-8bit/main.py
+++ pygame-8bit/main.py
@@ -14,10 +14,12 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
+        self.font = pygame.font.Font('assets/game_font.ttf', 32)
         self.groundsheet = Spritesheet('assets/gound4.png')
         self.character_spritesheet = Spritesheet('assets/character4.png')
         self.wallsheet = Spritesheet('assets/spikes.png')
         self.lavasheet = Spritesheet('assets/lava.png')
+        self.intro_background = pygame.image.load("assets/intro_background.png")
     def createTileMap(self, tilemap):
         build_map(self, tilemap)
 
@@ -55,7 +57,35 @@ class Game:
 
     def intro_screen(self, startresume):
         intro = True
-        
+
+        title = self.font.render("Main menu", True, "black")
+        title_rect = title.get_rect(x=200, y=100)
+
+        play_button = Button(SCREEN_WIDTH/2-BTN_W/2, 200, BTN_W, BTN_H, 'black', 'gray', f"{startresume} Game", 32)
+        exit_button = Button(SCREEN_WIDTH/2-BTN_W/2, 400, BTN_W, BTN_H, 'black', 'gray', "Exit Game", 32)
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                intro = False
+            elif exit_button.is_pressed(mouse_pos, mouse_pressed):
+                self.running = False
+                pygame.quit()
+                sys.exit()
+
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.screen.blit(exit_button.image, exit_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
+
 
 #Creacion del mapa y del bucle del juego
 TILEMAP = maps.world_1.stage_1
--- pygame-8bit/sprites.py
+++ pygame-8bit/sprites.py
@@ -59,3 +59,29 @@ class Lava(pygame.sprite.Sprite):
         self.image = self.game.lavasheet.sheet
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = self.x, self.y
+
+class Button:
+    def __init__(self, x , y, width, height, fg, bg, content, fontsize):
+        self.font = pygame.font.Font('assets/game_font.ttf', fontsize)
+        self.content = content
+
+        self.x, self.y = x, y
+        self.width, self.height = width, height
+
+        self.fg, self.bg = fg, bg
+        self.image = pygame.Surface((self.width,self.height))
+        self.image.fill(self.bg)
+        self.rect = self.image.get_rect()
+
+        self.rect.x, self.rect.y = self.x, self.y
+
+        self.text = self.font.render(self.content, True, self.fg)
+        self.text_rect = self.text.get_rect(center=(self.width/2, self.height/2))
+        self.image.blit(self.text, self.text_rect)
+
+    def is_pressed(self, pos, pressed):
+        if self.rect.collidepoint(pos):
+            if pressed[0]:
+                return True
+            return False
+        return False
