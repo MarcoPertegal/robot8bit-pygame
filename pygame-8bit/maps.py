c0bc5e25d1c47b42da785434834dc6cf753d0e7a
MarcoPertegal <marco.pertegal.prieto@gmail.com>
2024-02-01 13:48:25 +0100

    Place objets random

--- pygame-8bit/maps.py
+++ pygame-8bit/maps.py
@@ -1,5 +1,6 @@
 import random
 class world_1:
+    """
     def load_map(self):
         map_file = 'mapa.txt'
         with open('map.txt', 'r') as archivo:
@@ -7,11 +8,9 @@ class world_1:
             objets = {item.split(':')[0]: int(item.split(':')[1]) for item in first_line}
             map = [linea.strip() for linea in archivo]
 
-        return objets, map
+        return objets, map"""
 
-
-    """
-            def load_map(self):
+    def load_map(self):
         map_file = 'mapa.txt'
         with open('map.txt', 'r') as archivo:
             first_line = archivo.readline().strip().split(', ')
@@ -34,4 +33,4 @@ class world_1:
                 point_coords.remove(coord)
                 mapa[coord[1]] = mapa[coord[1]][:coord[0]] + obj + mapa[coord[1]][coord[0] + 1:]
 
-        return mapa"""
+        return objets, mapa
